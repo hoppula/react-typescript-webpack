00fe489cb1b781e7063e7d109bac22b001214cab
Lari Hoppula <lari@hoppula.com>
2017-05-17 21:48:55 +0300

    Bundle react & react-dom as well

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,34 @@
-var webpack = require("webpack");
-var path = require("path");
+var webpack = require("webpack")
+var path = require("path")
 
 module.exports = {
-    entry: [
-        "webpack-dev-server/client?http://localhost:8080/",
-        "webpack/hot/dev-server",
-        "./src/index.tsx"
-    ],
-    output: {
-        filename: "bundle.js",
-        publicPath: "/dist/",
-        path: path.resolve(__dirname, "dist")
-    },
+  entry: [
+    "webpack-dev-server/client?http://localhost:8080/",
+    "webpack/hot/dev-server",
+    "./src/index.tsx"
+  ],
+  output: {
+    filename: "bundle.js",
+    publicPath: "/dist/",
+    path: path.resolve(__dirname, "dist")
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, use: "awesome-typescript-loader" },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, enforce: "pre", use: "source-map-loader" }
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
-};
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, use: "awesome-typescript-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, enforce: "pre", use: "source-map-loader" }
+    ]
+  }
+}
