f724568dd30d0fa3ba688d00a749003bd24c9d37
Lari Hoppula <lari.hoppula@gofore.com>
2017-10-12 14:46:54 +0300

    Added prettier and tslint configs for prettier, use TSLint extension in your editor for auto-formatting

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,13 +1,17 @@
 import * as React from "react"
 
 export interface HelloProps {
-  compiler: string,
+  compiler: string
   framework: string
 }
 
 export class Hello extends React.Component<HelloProps, undefined> {
   render() {
     const { compiler, framework } = this.props
-    return <h1>Hello from {compiler} and {framework}!</h1>
+    return (
+      <h1>
+        Hello from {compiler} and {framework}!
+      </h1>
+    )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: [".ts", ".tsx", ".js"]
   },
 
   plugins: [new webpack.HotModuleReplacementPlugin()],
