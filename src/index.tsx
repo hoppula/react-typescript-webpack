215961abfb7fe1831013fc66a7e1e8737a33fd86
Lari Hoppula <lari.hoppula@gofore.com>
2017-10-12 14:56:39 +0300

    Added support for webpack hot module replacement

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,20 @@ import * as ReactDOM from "react-dom"
 
 import { Hello } from "./components/Hello"
 
+const exampleElement = document.getElementById("example")
+
 ReactDOM.render(
   <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById("example")
+  exampleElement
 )
+
+// webpack hot module replacement support
+if (module.hot) {
+  module.hot.accept("./components/Hello", () => {
+    const NextHello = require("./components/Hello").Hello
+    ReactDOM.render(
+      <NextHello compiler="TypeScript" framework="React" />,
+      exampleElement
+    )
+  })
+}
