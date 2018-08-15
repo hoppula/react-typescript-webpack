5526e3aefd5fd6f32c79182e52284d63e368fdec
Lari Hoppula <lari.hoppula@gofore.com>
2018-08-15 12:06:53 +0300

    Fixed no-implicit-dependencies tslint config for react-test-renderer

--- src/components/__tests__/Hello.test.tsx
+++ src/components/__tests__/Hello.test.tsx
@@ -4,6 +4,8 @@ import * as renderer from "react-test-renderer"
 import { Hello } from "../Hello"
 
 it("Hello renders correctly", () => {
-  const tree = renderer.create(<Hello compiler="TypeScript" framework="React" />).toJSON()
+  const tree = renderer
+    .create(<Hello compiler="TypeScript" framework="React" />)
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
